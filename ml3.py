5da94637f9bd687be7b5382d18cf85f145e1ec1a
riri-ctrl <richard.grabey@esme.fr>
2026-05-04 23:55:51 +0200

    Add files via upload

--- ml3.py
+++ ml3.py
@@ -285,15 +285,188 @@ def best_param(df,Y):
     
 #best_param(X_filtered,y)
 
-def ml_best_param (dataframe,result):
+def best_param(df,Y):
+    X_train, X_test, y_train, y_test = train_test_split(df,
+                                                        Y,
+                                                        test_size = 0.2,
+                                                        random_state =42,
+                                                        stratify =y)
+    
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    dt1 = DecisionTreeClassifier()
+    param = {
+        "criterion": ['gini', 'entropy'],
+        "max_depth": np.arange(2,15),
+        "min_samples_split": np.arange(2,15),
+        "min_samples_leaf": np.arange(1,10),
+        "max_features": [None, "sqrt", "log2"],
+        
+    }
     
     
-    X_train, X_test, y_train, y_test = train_test_split(dataframe, result, test_size = 0.8, random_state =42, stratify =result)
-   
+    grid = GridSearchCV(dt1,param_grid=param, cv=5)
+    grid.fit(X_train,y_train)
+    print(grid.best_params_)
+    print(grid.best_score_)
+    
+    #{'criterion': 'gini', 'max_depth': np.int64(9), 'max_features': None, 'min_samples_leaf': np.int64(7), 'min_samples_split': np.int64(2)}
+    #https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
+    
+    knn1=KNeighborsClassifier()
+    param = {
+        "n_neighbors": np.arange(1,15),
+        "weights": ['uniform', 'distance'],
+        "p": [1, 2]  # Manhattan vs Euclidean
+    }
+    
+    grid2 = GridSearchCV(knn1,param_grid=param, cv=5)
+    grid2.fit(X_train,y_train)
+    print(grid2.best_params_)
+    print(grid2.best_score_)
+    #{'n_neighbors': np.int64(14), 'p': 1, 'weights': 'uniform'}
+    #https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+    
+    lr1 = LogisticRegression()
+    param = [
+        {   
+             "max_iter":1500,  #====> max_iter ne change pas les résultats
+            "solver": ["lbfgs"],
+            "penalty": ["l2"],
+            "C": np.logspace(-3, 2, 6)
+        },
+        {   
+            "max_iter":1500,
+            "solver": ["saga"],
+            "penalty": ["elasticnet"],
+            "C": np.logspace(-3, 2, 6),
+            "l1_ratio": [0.2, 0.5, 0.8]
+        }
+    ]
+    
+    #penalty ne marche pas avec tous les solver
+        
+    grid3 = GridSearchCV(lr1,param_grid= param, cv=5)
+    grid3.fit(X_train,y_train)
+    print(grid3.best_params_)
+    print(grid3.best_score_)
+        
+    #{'C': np.float64(0.1), 'penalty': 'l2', 'solver': 'lbfgs'}
+    #https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+    
+    """
+    "n_estimators": [10,50, 75],
+    "criterion": ['gini', 'entropy'],    #demander la dif entre entropy et log_loss
+    "max_depth": [2,8,15],
+    "min_samples_split": [2,8,15],
+    "min_samples_leaf":[2,8,15],
+    "max_features": [ "sqrt", "log2"],
+    "class_weight": [None, "balanced"]
+    0.9333778371161549
+    
+    {'class_weight': None, 'criterion': 'entropy', 'max_depth': 15, 'max_features': 'log2', 'min_samples_leaf': 2, 'min_samples_split': 15, 'n_estimators': 75}
+    
+    "n_estimators": [50, 75,100],
+    "criterion": ['gini', 'entropy'],    
+    "max_depth": [10,15,20],
+    "min_samples_split": [10,15,20],
+    "min_samples_leaf":[2,4,8],
+    "max_features": [ "sqrt", "log2"],
+    "class_weight": [None, "balanced"]
+    {'class_weight': None, 'criterion': 'gini', 'max_depth': 10, 'max_features': 'log2', 'min_samples_leaf': 2, 'min_samples_split': 20, 'n_estimators': 100}
+    0.9336448598130841
+    
+    "n_estimators": [75,100,150],
+    "criterion": ['gini', 'entropy'],    
+    "max_depth": [5,10,15,20],
+    "min_samples_split": [15,20,25],
+    "min_samples_leaf":[2,3,4],
+    "max_features": [ "sqrt", "log2"],
+    "class_weight": [None, "balanced"]
+    {'class_weight': None, 'criterion': 'entropy', 'max_depth': 20, 'max_features': 'log2', 'min_samples_leaf': 2, 'min_samples_split': 15, 'n_estimators': 75}
+    0.9336893635959056
+    
+    "n_estimators": [75,85,100],
+    "criterion": ['gini', 'entropy'],    
+    "max_depth": [10,13,15,17,20],
+    "min_samples_split": [15,17,20],
+    "min_samples_leaf":[2,3],
+    "max_features": [ "sqrt", "log2"],
+    "class_weight": [None, "balanced"]
+    {'class_weight': None, 'criterion': 'gini', 'max_depth': 13, 'max_features': 'log2', 'min_samples_leaf': 3, 'min_samples_split': 15, 'n_estimators': 85}
+    0.9338673787271917
+    
+    "n_estimators": [75,80,85,90,100],
+    "criterion": ['gini', 'entropy'],    
+    "max_depth": [10,11,12,13,14],
+    "min_samples_split": [13,15,17],
+    "min_samples_leaf":[2,3,4],
+    "max_features": ["log2"],
+    "class_weight": [None]
+    {'class_weight': None, 'criterion': 'entropy', 'max_depth': 12, 'max_features': 'log2', 'min_samples_leaf': 4, 'min_samples_split': 17, 'n_estimators': 90}
+    0.9337338673787272
     
     
+    "n_estimators": np.arange(80,100,2),
+    "criterion": ['gini', 'entropy'],    
+    "max_depth": np.arange(10,15),
+    "min_samples_split": np.arange(13,20),
+    "min_samples_leaf":np.arange(2,5),
+    "max_features": ["log2"],
+    "class_weight": [None]
+    {'class_weight': None, 'criterion': 'entropy', 'max_depth': np.int64(13), 'max_features': 'log2', 'min_samples_leaf': np.int64(3), 'min_samples_split': np.int64(15), 'n_estimators': np.int64(98)}
+    0.9342679127725857
+    """
+    rd1 = RandomForestClassifier()
+    param = {
+        
+        "n_estimators": np.arange(90,100),
+        "criterion": ['gini', 'entropy'],    
+        "max_depth": np.arange(10,15),
+        "min_samples_split": np.arange(13,20),
+        "min_samples_leaf":np.arange(2,5),
+        "max_features": ["log2"],
+        "class_weight": [None]
+        
+        
+        }
+    
+    grid4 = GridSearchCV(rd1,param_grid= param, cv=3,n_jobs=-1)
+    grid4.fit(X_train,y_train)
+    print(grid4.best_params_)
+    print(grid4.best_score_)
+    #{'class_weight': None, 'criterion': 'gini', 'max_depth': np.int64(12), 'max_features': 'log2', 'min_samples_leaf': np.int64(2), 'min_samples_split': np.int64(14), 'n_estimators': np.int64(99)}
+    #0.9337338673787272
     
     
+#best_param(X_filtered,y)
+
+def seuil(proba,y_test,model):
+    """
+    pred = vc.predict(X_test)
+    print("Accuracy Voting :", accuracy_score(y_test, pred))
+    """
+    
+    for t in [0.3, 0.4, 0.5,0.6]:
+        
+        pred = (proba > t).astype(int)
+        
+        print(f"\nSeuil = {t}")
+        print(f"{model} :", classification_report(y_test, pred))
+        cm=confusion_matrix(y_test, pred, labels=[0, 1])
+        tn, fp, fn, tp = cm.ravel()
+        print(f"Vrais négatifs (TN) : {tn}")
+        print(f"Faux positifs (FP) : {fp}")
+        print(f"Faux négatifs (FN) : {fn}")
+        print(f"Vrais positifs (TP) : {tp}")
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Négatif", "Positif"])
+        disp.plot(cmap=plt.cm.Blues, values_format='d')  # 'd' = entier
+        plt.title("Matrice de confusion")
+        plt.show()
+
+def init_model_best_para():
     dt = DecisionTreeClassifier(
        criterion='gini',
        max_depth=9,
@@ -330,18 +503,10 @@ def ml_best_param (dataframe,result):
            solver='lbfgs'
        ))
    ])
+    return dt,rd,knn,lr
+    
 
-    
-    
-    
-    
-    models = {
-        "dt":dt,
-        "Rd": rd,
-        "LR": lr,
-        "KNN": knn
-    }
-    
+def ROC(models,X_train, X_test, y_train, y_test):
     for name, model in models.items():
         model.fit(X_train, y_train)
         pred = model.predict(X_test)
@@ -358,6 +523,23 @@ def ml_best_param (dataframe,result):
     plt.legend()
     plt.show()
     
+def ml_best_param (dataframe,result):
+    
+    
+    X_train, X_test, y_train, y_test = train_test_split(dataframe, result, test_size = 0.8, random_state =42, stratify =result)
+    
+    
+    dt,rd,knn,lr=init_model_best_para()
+    
+    models = {
+        "dt":dt,
+        "Rd": rd,
+        "LR": lr,
+        "KNN": knn
+    }
+    
+    ROC(models,X_train, X_test, y_train, y_test)
+    
     classifiers = [
        
        ('foret', rd),
@@ -370,29 +552,12 @@ def ml_best_param (dataframe,result):
     )
     
     vc.fit(X_train, y_train)
-    """
-    pred = vc.predict(X_test)
-    print("Accuracy Voting :", accuracy_score(y_test, pred))
-    """
+    
     proba = vc.predict_proba(X_test)[:,1]
-    for t in [0.3, 0.4, 0.5,0.6]:
+    seuil(proba,y_test,vc)
+    
         
-        pred = (proba > t).astype(int)
-        
-        print(f"\nSeuil = {t}")
-        print(f"{model} :", classification_report(y_test, pred))
-        cm=confusion_matrix(y_test, pred, labels=[0, 1])
-        tn, fp, fn, tp = cm.ravel()
-        print(f"Vrais négatifs (TN) : {tn}")
-        print(f"Faux positifs (FP) : {fp}")
-        print(f"Faux négatifs (FN) : {fn}")
-        print(f"Vrais positifs (TP) : {tp}")
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Classe 0", "Classe 1"])
-        disp.plot(cmap=plt.cm.Blues, values_format='d')  # 'd' = entier
-        plt.title("Matrice de confusion")
-        plt.show()
-        
-    joblib.dump(vc, "voting.pkl")
+    joblib.dump(vc, "voting", compress=3)
     return vc
     
     
